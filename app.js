2ea9c62e1c4dd95e2842b843bb6adc0a5507ddc2
lplp322 <ivan.kravcev@gmail.com>
2022-01-13 18:08:39 +0100

    ws addaed, app changed

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/*************** 
+      Required modules
+*****************/
+const express = require("express");
+const res = require("express/lib/response");
+const websocket = require("ws");
+const http = require("http");
+const Game = require("./game");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const port = process.argv[2]; //connection to the port(provided in the second argument)
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(express.static(__dirname + "/public")); // allow to access public files directly
+app.get("/", (req, res)=>{
+  res.sendFile("splash.html" , {root: "./public"});
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get("/play", (req, res)=>{
+  res.sendFile("game.html" , {root: "./public"});
 });
+const server = http.createServer(app);
 
-module.exports = app;
+/***************
+Web Socket 
+*****************/
+const wss = new websocket.Server({server});
+let numberOfPlayers = 0;
+wss.on("connection", function (ws) {
+  numberOfPlayers++;
+  console.log(`Player with ID: ${numberOfPlayers} connected. Connection state: ${ws.readyState}`);
+  ws.send(`ID:${numberOfPlayers}`);
+
+  ws.on("message", function incoming(message) {
+    console.log("[LOG] " + message);
+    if(message == "START-GAME"){
+      console.log("Starting game");
+      ws.send("WAITING-FOR-OTHER-PLAYER");
+    }
+  });
+});
+server.listen(port); 
