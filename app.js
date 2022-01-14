e4db8b041e075d55ac1c6e40b9c935324d868139
lplp322 <ivan.kravcev@gmail.com>
2022-01-14 17:52:15 +0100

    new

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const game = function(gameID) {
-  this.playerA = null;
-  this.playerB = null;
-  this.id = gameID;
-  this.blackPos = [0,0,0,0];
-  this.bluePos = [0,0,0,0];
-  this.gameState = "0 JOINT"; //"A" means A won, "B" means B won, "ABORTED" means the game was aborted
-  this.addPlayer = function(w){
-      if(this.playerA==null){
-        this.playerA = w;
-        return 1;
-      }
-      else{
-        this.playerB = w;
-        return 2;
-      }
-  }
-  this.isYourTURN = function(w){
-    return true;
-  }
-  this.rollDice = function(){
-    return 5;
-  }
-  this.madeMove = function(pos){ //return move from the provided position or "Incorrect move" if the move is incorrect
-    const diceNumb = 5;
-    return "MOVE:15-21";
-  }
-};
+// const game = function(gameID) {
+//   this.playerA = null;
+//   this.playerB = null;
+//   this.id = gameID;
+//   this.blackPos = [0,0,0,0];
+//   this.bluePos = [0,0,0,0];
+//   this.gameState = "0 JOINT"; //"A" means A won, "B" means B won, "ABORTED" means the game was aborted
+//   this.addPlayer = function(w){
+//       if(this.playerA==null){
+//         this.playerA = w;
+//         return 1;
+//       }
+//       else{
+//         this.playerB = w;
+//         return 2;
+//       }
+//   }
+//   this.isYourTURN = function(w){
+//     return true;
+//   }
+//   this.rollDice = function(){
+//     return 5;
+//   }
+//   this.madeMove = function(pos){ //return move from the provided position or "Incorrect move" if the move is incorrect
+//     const diceNumb = 5;
+//     return "MOVE:15-21";
+//   }
+// };
 
 /*************** 
       Required modules
@@ -35,12 +35,11 @@ const res = require("express/lib/response");
 const websocket = require("ws");
 const http = require("http");
 const messages = require("./public/javascripts/messages");
-//const Game = require("./Game");
-
+const Game = require("./scripts/Game");
 
 const port = process.argv[2]; //connection to the port(provided in the second argument)
 const app = express();
-const websockets = {} // object for storing games
+const websockets = {}; // object for storing games
 
 app.use(express.static(__dirname + "/public")); // allow to access public files directly
 // app.get("/", (req, res)=>{
@@ -54,48 +53,45 @@ const server = http.createServer(app);
 /***************
 Web Socket 
 *****************/
-const wss = new websocket.Server({server});
+const wss = new websocket.Server({ server });
 let numberOfPlayers = 0;
-let gameID=0;
-let currentGame = new game(gameID);
-
+let gameID = 0;
+let currentGame = new Game();
 
 //CLEANING OF THE GAME OBJECTS NEEDED TO BE DONE
 
 wss.on("connection", function (ws) {
-
   //starting of the game
   const con = ws;
   con["id"] = numberOfPlayers++;
   const playerType = currentGame.addPlayer(con); // returning type of the player
   websockets[con["id"]] = currentGame;
 
-  console.log(
-    `Player ${con["id"]} placed in game ${currentGame.id} as ${playerType}`
-  );
-  con.send(playerType == 1 ? messages.S_PLAYER_1: messages.S_PLAYER_2);
-  if(playerType == 1){
+  console.log(`Player ${con["id"]} placed in game ${gameID} as ${playerType}`);
+  con.send(playerType == 1 ? messages.S_PLAYER_1 : messages.S_PLAYER_2);
+  if (playerType == 1) {
     con.send(messages.S_WAIT);
   }
-  if(playerType == 2){
+  if (playerType == 2) {
     const opponent = currentGame.getOpponentOf(con);
     opponent.send(messages.S_START);
     con.send(messages.S_START);
     opponent.send(messages.S_YOUR_TURN);
     con.send(messages.S_OPP_TURN);
-    currentGame = new game(++gameID);
+    currentGame = new Game();
+    gameID++;
   }
 
   //response messages
   con.on("message", function incoming(message) {
-
     const oMsg = JSON.parse(message.toString());
     const gameObj = websockets[con["id"]];
     const opponent = gameObj.getOpponentOf(con);
 
-    if(gameObj.isYourTURN(con)){ //can do it only if it is your turn
+    if (gameObj.isTurnOf(con)) {
+      //can do it only if it is your turn
       //dice rolled
-      if(oMsg.type == messages.T_DICE_ROLLED){ 
+      if (oMsg.type == messages.T_DICE_ROLLED) {
         const dice = gameObj.rollDice();
         let response = messages.O_YOU_ROLLED;
         response.data = dice.number;
@@ -106,15 +102,15 @@ wss.on("connection", function (ws) {
         opponent.send(JSON.stringify(oppResp));
       }
       //move somewhere
-      else if(oMsg.type = messages.T_CLIENT_MOVE){
+      else if ((oMsg.type = messages.T_CLIENT_MOVE)) {
         const move = oMsg.from;
         const madeMove = gameObj.madeMove(move);
         //INVALID MOVE
-        if(madeMove == "Invalid move"){
+        if (madeMove == "InvalidMove") {
           con.send(messages.S_INVALID);
         }
-        //NORMAL MOVE 
-        else{
+        //NORMAL MOVE
+        else {
           let response = messages.O_MOVE;
           response.from = madeMove.from;
           response.to = madeMove.to;
@@ -122,23 +118,20 @@ wss.on("connection", function (ws) {
           con.send(JSON.stringify(response));
           opponent.send(JSON.stringify(response));
 
-
           //check for the move changing
-          if(gameObj.isTurnOf(con)){
+          if (gameObj.isTurnOf(con)) {
             con.send(messages.S_YOUR_TURN);
             opponent.send(messages.S_OPP_TURN);
-          }
-          else{
+          } else {
             con.send(messages.S_OPP_TURN);
             opponent.send(messages.S_YOUR_TURN);
           }
           //check for the winning situation needed to be implemented
-          
         }
       }
     }
   });
-  con.on("close", function(code){
+  con.on("close", function (code) {
     // const gameObj = websockets[con["id"]];
     // const opponent = gameObj.getOpponentOf(con);
     console.log(`Player ${con["id"]} disconnected ...`);
@@ -148,7 +141,7 @@ wss.on("connection", function (ws) {
        */
       const gameObj = websockets[con["id"]];
       const opponent = gameObj.getOpponentOf(con);
-      if(opponent!=null){
+      if (opponent != null) {
         opponent.send(messages.S_ABORTED);
         try {
           opponent.close();
@@ -161,4 +154,4 @@ wss.on("connection", function (ws) {
     }
   });
 });
-server.listen(port); 
+server.listen(port);
